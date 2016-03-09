bc302f4099a54edb3e575c4e42ebdbf295387abc
wng1 <wng1@users.noreply.github.com>
2016-03-09 01:37:32 +0000

    Update HomeController.java

--- src/main/controller/HomeController.java
+++ src/main/controller/HomeController.java
@@ -14,26 +14,6 @@ model.addAttribute("intro", "Welcome to the Supply Chain Management System!");
 model.addAttribute("brief", "Fast, Quick and Reliable service");
 
 return "welcome";
-
-
-
-//ProductController.java
-
-@Controller
-
-public class ProductController
-{
-@Autowired
-private IProductService ips;
-
-@RequestMapping("/product")
-public String getPID(@RequestParam("pID") String pID, Model model)
-{
-Product p =  ips.getPID(pID); //This gets the method from the interface class and create an object that will be added to the next attr
-model.addAttribute("product", p);
-return "product";
-}
-
 }
 
 }
